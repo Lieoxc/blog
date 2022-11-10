521bd5adee5ada786a11dab7eaed46679e1147cf
lieoxc <xiaocongli45@gmail.com>
2022-11-10 23:30:08 +0800

    图片存储使用七牛云调试通过

--- control/sysctl/sysctl.go
+++ control/sysctl/sysctl.go
@@ -4,11 +4,7 @@ import (
 	"blog/model"
 	_ "image/gif"
 	_ "image/png"
-	"io"
-	"os"
-	"path"
 	"runtime"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zxysilent/utils"
@@ -45,44 +41,6 @@ func StatusAppinfo(ctx *gin.Context) {
 	ctx.JSON(utils.Fail(`未查询到统计信息`))
 }
 
-// UploadFile doc
-// @Tags ctrl-系统相关
-// @Summary 上传文件
-// @Accept  mpfd
-// @Param token query string true "token"
-// @Param file formData file true "file"
-// @Router /adm/upload/file [post]
-func UploadFile(ctx *gin.Context) {
-	file, err := ctx.FormFile("file")
-	if err != nil {
-		ctx.JSON(utils.Fail("未发现文件", err.Error()))
-		return
-	}
-	src, err := file.Open()
-	if err != nil {
-		ctx.JSON(utils.Fail("文件打开失败", err.Error()))
-		return
-
-	}
-	dir := time.Now().Format("200601/02")
-	os.MkdirAll("./webui/static/upload/"+dir[:6], 0755)
-	name := "webui/static/upload/" + dir + utils.RandStr(10) + path.Ext(file.Filename)
-	dst, err := os.Create(name)
-	if err != nil {
-		ctx.JSON(utils.Fail("文件打创建文件失败", err.Error()))
-		return
-
-	}
-	_, err = io.Copy(dst, src)
-	if err != nil {
-		ctx.JSON(utils.Fail("文件保存失败", err.Error()))
-		return
-	}
-	src.Close()
-	dst.Close()
-	ctx.JSON(utils.Succ("上传成功", "/"+name))
-}
-
 // UploadImage doc
 // @Tags ctrl-系统相关
 // @Summary 上传图片并裁剪
@@ -90,7 +48,7 @@ func UploadFile(ctx *gin.Context) {
 // @Param token query string true "token"
 // @Param file formData file true "file"
 // @Router /adm/upload/image [post]
-func UploadImage(c *gin.Context) {
+func UploadFile(c *gin.Context) {
 	file, fileHeader, _ := c.Request.FormFile("file")
 
 	fileSize := fileHeader.Size
